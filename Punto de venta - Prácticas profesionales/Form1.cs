863d2ee0b67f8048164446e22f1f283dd135b355
Canseco Gabriel <alexander95.gac@gmail.com>
2024-10-26 16:07:58 -0300

    Agregar archivos de proyecto.

---
+++ Punto de venta - Prácticas profesionales/Form1.cs
@@ -0,0 +1,10 @@
+namespace Punto_de_venta___Prácticas_profesionales
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
