eeb2929a2431fce72b920792071abe590c9e7f96
Canseco Gabriel <alexander95.gac@gmail.com>
2024-11-05 17:31:26 -0300

    Finalizado diseño general de Proveedores

--- Punto de venta - Prácticas profesionales/Presentación/FormProveedores.cs
+++ Punto de venta - Prácticas profesionales/Presentación/FormProveedores.cs
@@ -16,5 +16,24 @@ namespace Punto_de_venta___Prácticas_profesionales.Presentación
         {
             InitializeComponent();
         }
+
+        private void FormProveedores_Resize(object sender, EventArgs e)
+        {
+
+                int baseWidth = 800; // Your base form width
+                int baseHeight = 600; // Your base form height
+
+                float widthRatio = (float)this.Width / baseWidth;
+                float heightRatio = (float)this.Height / baseHeight;
+
+                foreach (Control control in this.Controls)
+                {
+                    control.Width = (int)(control.Width * widthRatio);
+                    control.Height = (int)(control.Height * heightRatio);
+                    control.Left = (int)(control.Left * widthRatio);
+                    control.Top = (int)(control.Top * heightRatio);
+                }
+
+        }
     }
 }
