a607de5ced52318178f9d3a78f33aa10cc8b6805
sofia aguirre <aguirreanasofia07@gmail.com>
2024-11-07 18:59:47 -0300

    agregada la clase de texboxs personalizados y creados los tex box para su reutilizacion

---
+++ Punto de venta - Prácticas profesionales/controlesPersonalizados/TexBoxPersonalizad.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace Punto_de_venta___Prácticas_profesionales.controlesPersonalizados
+{
+    partial class TexBoxPersonalizad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            texboxs1 = new Texboxs();
+            texboxs2 = new Texboxs();
+            texboxs3 = new Texboxs();
+            texboxs5 = new Texboxs();
+            texboxs6 = new Texboxs();
+            texboxs7 = new Texboxs();
+            texboxs8 = new Texboxs();
+            texboxs9 = new Texboxs();
+            texboxs10 = new Texboxs();
+            label1 = new Label();
+            Codigo = new Label();
+            label2 = new Label();
+            texboxs4 = new Texboxs();
+            SuspendLayout();
+            // 
+            // texboxs1
+            // 
+            texboxs1.BackColor = Color.Gainsboro;
+            texboxs1.BorderColor = Color.Black;
+            texboxs1.BorderFocusColor = Color.DarkGray;
+            texboxs1.BorderSize = 2;
+            texboxs1.Font = new Font("Segoe UI", 9.5F);
+            texboxs1.ForeColor = Color.DimGray;
+            texboxs1.Location = new Point(97, 49);
+            texboxs1.Name = "texboxs1";
+            texboxs1.Padding = new Padding(7);
+            texboxs1.Size = new Size(126, 32);
+            texboxs1.TabIndex = 0;
+            texboxs1.Texts = "";
+            texboxs1.UnderlinedStyle1 = true;
+            texboxs1._TextChanged += texboxs1__TextChanged;
+            // 
+            // texboxs2
+            // 
+            texboxs2.BackColor = SystemColors.Window;
+            texboxs2.BorderColor = Color.MediumBlue;
+            texboxs2.BorderFocusColor = Color.DarkGray;
+            texboxs2.BorderSize = 2;
+            texboxs2.Font = new Font("Segoe UI", 9.5F);
+            texboxs2.ForeColor = Color.DimGray;
+            texboxs2.Location = new Point(440, 81);
+            texboxs2.Name = "texboxs2";
+            texboxs2.Padding = new Padding(7);
+            texboxs2.Size = new Size(165, 32);
+            texboxs2.TabIndex = 1;
+            texboxs2.Texts = "";
+            texboxs2.UnderlinedStyle1 = false;
+            // 
+            // texboxs3
+            // 
+            texboxs3.BackColor = Color.Gainsboro;
+            texboxs3.BorderColor = Color.FromArgb(64, 64, 64);
+            texboxs3.BorderFocusColor = Color.DarkGray;
+            texboxs3.BorderSize = 2;
+            texboxs3.Font = new Font("Segoe UI", 9.5F);
+            texboxs3.ForeColor = Color.DimGray;
+            texboxs3.Location = new Point(97, 87);
+            texboxs3.Name = "texboxs3";
+            texboxs3.Padding = new Padding(7);
+            texboxs3.Size = new Size(126, 32);
+            texboxs3.TabIndex = 2;
+            texboxs3.Texts = "";
+            texboxs3.UnderlinedStyle1 = true;
+            // 
+            // texboxs5
+            // 
+            texboxs5.BackColor = SystemColors.Window;
+            texboxs5.BorderColor = Color.MediumBlue;
+            texboxs5.BorderFocusColor = Color.DarkGray;
+            texboxs5.BorderSize = 1;
+            texboxs5.Font = new Font("Segoe UI", 9.5F);
+            texboxs5.ForeColor = Color.DimGray;
+            texboxs5.Location = new Point(440, 129);
+            texboxs5.Name = "texboxs5";
+            texboxs5.Padding = new Padding(7);
+            texboxs5.Size = new Size(173, 32);
+            texboxs5.TabIndex = 4;
+            texboxs5.Texts = "";
+            texboxs5.UnderlinedStyle1 = false;
+            // 
+            // texboxs6
+            // 
+            texboxs6.BackColor = SystemColors.Window;
+            texboxs6.BorderColor = Color.Snow;
+            texboxs6.BorderFocusColor = Color.DarkGray;
+            texboxs6.BorderSize = 1;
+            texboxs6.Font = new Font("Segoe UI", 9.5F);
+            texboxs6.ForeColor = Color.DimGray;
+            texboxs6.Location = new Point(344, 292);
+            texboxs6.Name = "texboxs6";
+            texboxs6.Padding = new Padding(7);
+            texboxs6.Size = new Size(160, 32);
+            texboxs6.TabIndex = 5;
+            texboxs6.Texts = "";
+            texboxs6.UnderlinedStyle1 = false;
+            // 
+            // texboxs7
+            // 
+            texboxs7.BackColor = Color.Gainsboro;
+            texboxs7.BorderColor = Color.Black;
+            texboxs7.BorderFocusColor = Color.DarkGray;
+            texboxs7.BorderSize = 1;
+            texboxs7.Font = new Font("Segoe UI", 9.5F);
+            texboxs7.ForeColor = Color.DimGray;
+            texboxs7.Location = new Point(140, 254);
+            texboxs7.Name = "texboxs7";
+            texboxs7.Padding = new Padding(7);
+            texboxs7.Size = new Size(152, 32);
+            texboxs7.TabIndex = 6;
+            texboxs7.Texts = "";
+            texboxs7.UnderlinedStyle1 = false;
+            // 
+            // texboxs8
+            // 
+            texboxs8.BackColor = Color.Gainsboro;
+            texboxs8.BorderColor = Color.Black;
+            texboxs8.BorderFocusColor = Color.DarkGray;
+            texboxs8.BorderSize = 1;
+            texboxs8.Font = new Font("Segoe UI", 9.5F);
+            texboxs8.ForeColor = Color.DimGray;
+            texboxs8.Location = new Point(140, 292);
+            texboxs8.Name = "texboxs8";
+            texboxs8.Padding = new Padding(7);
+            texboxs8.Size = new Size(152, 32);
+            texboxs8.TabIndex = 7;
+            texboxs8.Texts = "";
+            texboxs8.UnderlinedStyle1 = false;
+            // 
+            // texboxs9
+            // 
+            texboxs9.BackColor = Color.Gainsboro;
+            texboxs9.BorderColor = Color.MediumBlue;
+            texboxs9.BorderFocusColor = Color.DarkGray;
+            texboxs9.BorderSize = 2;
+            texboxs9.Font = new Font("Segoe UI", 9.5F);
+            texboxs9.ForeColor = Color.DimGray;
+            texboxs9.Location = new Point(529, 292);
+            texboxs9.Name = "texboxs9";
+            texboxs9.Padding = new Padding(7);
+            texboxs9.Size = new Size(154, 32);
+            texboxs9.TabIndex = 8;
+            texboxs9.Texts = "";
+            texboxs9.UnderlinedStyle1 = true;
+            // 
+            // texboxs10
+            // 
+            texboxs10.BackColor = Color.Gainsboro;
+            texboxs10.BorderColor = Color.MediumBlue;
+            texboxs10.BorderFocusColor = Color.DarkGray;
+            texboxs10.BorderSize = 2;
+            texboxs10.Font = new Font("Segoe UI", 9.5F);
+            texboxs10.ForeColor = Color.DimGray;
+            texboxs10.Location = new Point(264, 129);
+            texboxs10.Name = "texboxs10";
+            texboxs10.Padding = new Padding(7);
+            texboxs10.Size = new Size(154, 32);
+            texboxs10.TabIndex = 9;
+            texboxs10.Texts = "";
+            texboxs10.UnderlinedStyle1 = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial Black", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(22, 63);
+            label1.Name = "label1";
+            label1.Size = new Size(69, 18);
+            label1.TabIndex = 10;
+            label1.Text = "Nombre:";
+            // 
+            // Codigo
+            // 
+            Codigo.AutoSize = true;
+            Codigo.Font = new Font("Arial Black", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Codigo.Location = new Point(22, 101);
+            Codigo.Name = "Codigo";
+            Codigo.Size = new Size(62, 18);
+            Codigo.TabIndex = 11;
+            Codigo.Text = "Codigo:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial Black", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(50, 268);
+            label2.Name = "label2";
+            label2.Size = new Size(69, 18);
+            label2.TabIndex = 12;
+            label2.Text = "Nombre:";
+            // 
+            // texboxs4
+            // 
+            texboxs4.BackColor = SystemColors.Window;
+            texboxs4.BorderColor = Color.MediumBlue;
+            texboxs4.BorderFocusColor = Color.DarkGray;
+            texboxs4.BorderSize = 1;
+            texboxs4.Font = new Font("Segoe UI", 9.5F);
+            texboxs4.ForeColor = Color.DimGray;
+            texboxs4.Location = new Point(344, 245);
+            texboxs4.Name = "texboxs4";
+            texboxs4.Padding = new Padding(7);
+            texboxs4.Size = new Size(160, 32);
+            texboxs4.TabIndex = 13;
+            texboxs4.Texts = "";
+            texboxs4.UnderlinedStyle1 = false;
+            // 
+            // TexBoxPersonalizad
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Gainsboro;
+            ClientSize = new Size(800, 450);
+            Controls.Add(texboxs4);
+            Controls.Add(label2);
+            Controls.Add(Codigo);
+            Controls.Add(label1);
+            Controls.Add(texboxs10);
+            Controls.Add(texboxs9);
+            Controls.Add(texboxs8);
+            Controls.Add(texboxs7);
+            Controls.Add(texboxs6);
+            Controls.Add(texboxs5);
+            Controls.Add(texboxs3);
+            Controls.Add(texboxs2);
+            Controls.Add(texboxs1);
+            Name = "TexBoxPersonalizad";
+            Text = "TexBoxPersonalizad";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Texboxs texboxs1;
+        private Texboxs texboxs2;
+        private Texboxs texboxs3;
+        private Texboxs texboxs5;
+        private Texboxs texboxs6;
+        private Texboxs texboxs7;
+        private Texboxs texboxs8;
+        private Texboxs texboxs9;
+        private Texboxs texboxs10;
+        private Label label1;
+        private Label Codigo;
+        private Label label2;
+        private Texboxs texboxs4;
+    }
+}
