9c6e1bba049332b891391eb72b6926172b066a55
Favri <albarracinfabri@gmail.com>
2024-11-17 19:21:44 -0300

    Ventas

--- Punto de venta - Prácticas profesionales/Presentación/FormVentas.Designer.cs
+++ Punto de venta - Prácticas profesionales/Presentación/FormVentas.Designer.cs
@@ -28,32 +28,391 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
+            dataGridView1 = new DataGridView();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            groupBox1 = new GroupBox();
+            texboxs4 = new controlesPersonalizados.Texboxs();
+            texboxs3 = new controlesPersonalizados.Texboxs();
+            groupBox2 = new GroupBox();
+            texboxs5 = new controlesPersonalizados.Texboxs();
+            comboBox1 = new ComboBox();
+            classBtnPersonalizado2 = new controlesPersonalizados.ClassBtnPersonalizado();
+            classBtnPersonalizado1 = new controlesPersonalizados.ClassBtnPersonalizado();
+            classBtnPersonalizado3 = new controlesPersonalizados.ClassBtnPersonalizado();
+            groupBox3 = new GroupBox();
+            comboBox3 = new ComboBox();
+            label12 = new Label();
+            comboBox2 = new ComboBox();
+            comboBox4 = new ComboBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
             SuspendLayout();
             // 
-            // label1
+            // dataGridView1
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(360, 115);
-            label1.Name = "label1";
-            label1.Size = new Size(116, 15);
-            label1.TabIndex = 1;
-            label1.Text = "formulario de ventas";
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(118, 94);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(720, 257);
+            dataGridView1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(118, 63);
+            label2.Name = "label2";
+            label2.Size = new Size(44, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Cliente";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(607, 68);
+            label3.Name = "label3";
+            label3.Size = new Size(57, 15);
+            label3.TabIndex = 3;
+            label3.Text = "Vendedor";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(9, 31);
+            label4.Name = "label4";
+            label4.Size = new Size(55, 15);
+            label4.TabIndex = 4;
+            label4.Text = "Sub Total";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(6, 55);
+            label5.Name = "label5";
+            label5.Size = new Size(58, 32);
+            label5.TabIndex = 5;
+            label5.Text = "0.00";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(120, 31);
+            label6.Name = "label6";
+            label6.Size = new Size(57, 15);
+            label6.TabIndex = 6;
+            label6.Text = "Impuesto";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(120, 69);
+            label7.Name = "label7";
+            label7.Size = new Size(63, 15);
+            label7.TabIndex = 7;
+            label7.Text = "Descuento";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(24, 97);
+            label8.Name = "label8";
+            label8.Size = new Size(32, 15);
+            label8.TabIndex = 8;
+            label8.Text = "Total";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 21.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label9.Location = new Point(46, 121);
+            label9.Name = "label9";
+            label9.Size = new Size(71, 40);
+            label9.TabIndex = 9;
+            label9.Text = "0.00";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(26, 19);
+            label10.Name = "label10";
+            label10.Size = new Size(51, 15);
+            label10.TabIndex = 10;
+            label10.Text = "Nombre";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(135, 19);
+            label11.Name = "label11";
+            label11.Size = new Size(55, 15);
+            label11.TabIndex = 11;
+            label11.Text = "Cantidad";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(texboxs4);
+            groupBox1.Controls.Add(texboxs3);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label9);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Location = new Point(118, 375);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(273, 179);
+            groupBox1.TabIndex = 12;
+            groupBox1.TabStop = false;
+            // 
+            // texboxs4
+            // 
+            texboxs4.BackColor = SystemColors.Window;
+            texboxs4.BorderColor = Color.MediumBlue;
+            texboxs4.BorderFocusColor = Color.DarkGray;
+            texboxs4.BorderSize = 2;
+            texboxs4.Font = new Font("Segoe UI", 9.5F);
+            texboxs4.ForeColor = Color.DimGray;
+            texboxs4.Location = new Point(185, 64);
+            texboxs4.Name = "texboxs4";
+            texboxs4.Padding = new Padding(7);
+            texboxs4.Size = new Size(53, 32);
+            texboxs4.TabIndex = 20;
+            texboxs4.Texts = "";
+            texboxs4.UnderlinedStyle1 = false;
+            // 
+            // texboxs3
+            // 
+            texboxs3.BackColor = SystemColors.Window;
+            texboxs3.BorderColor = Color.MediumBlue;
+            texboxs3.BorderFocusColor = Color.DarkGray;
+            texboxs3.BorderSize = 2;
+            texboxs3.Font = new Font("Segoe UI", 9.5F);
+            texboxs3.ForeColor = Color.DimGray;
+            texboxs3.Location = new Point(185, 19);
+            texboxs3.Name = "texboxs3";
+            texboxs3.Padding = new Padding(7);
+            texboxs3.Size = new Size(53, 32);
+            texboxs3.TabIndex = 19;
+            texboxs3.Texts = "";
+            texboxs3.UnderlinedStyle1 = false;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(texboxs5);
+            groupBox2.Controls.Add(comboBox1);
+            groupBox2.Controls.Add(classBtnPersonalizado2);
+            groupBox2.Controls.Add(classBtnPersonalizado1);
+            groupBox2.Controls.Add(label10);
+            groupBox2.Controls.Add(label11);
+            groupBox2.Location = new Point(425, 375);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(225, 179);
+            groupBox2.TabIndex = 13;
+            groupBox2.TabStop = false;
+            // 
+            // texboxs5
+            // 
+            texboxs5.BackColor = SystemColors.Window;
+            texboxs5.BorderColor = Color.MediumBlue;
+            texboxs5.BorderFocusColor = Color.DarkGray;
+            texboxs5.BorderSize = 2;
+            texboxs5.Font = new Font("Segoe UI", 9.5F);
+            texboxs5.ForeColor = Color.DimGray;
+            texboxs5.Location = new Point(135, 37);
+            texboxs5.Name = "texboxs5";
+            texboxs5.Padding = new Padding(7);
+            texboxs5.Size = new Size(65, 32);
+            texboxs5.TabIndex = 21;
+            texboxs5.Texts = "";
+            texboxs5.UnderlinedStyle1 = false;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(9, 43);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(104, 23);
+            comboBox1.TabIndex = 15;
+            comboBox1.TextChanged += comboBox1_TextChanged;
+            // 
+            // classBtnPersonalizado2
+            // 
+            classBtnPersonalizado2.BackColor = Color.MediumSlateBlue;
+            classBtnPersonalizado2.BackgroundColor = Color.MediumSlateBlue;
+            classBtnPersonalizado2.BorderColor = Color.PaleVioletRed;
+            classBtnPersonalizado2.BorderRadius = 40;
+            classBtnPersonalizado2.BorderSize = 0;
+            classBtnPersonalizado2.FlatAppearance.BorderSize = 0;
+            classBtnPersonalizado2.FlatStyle = FlatStyle.Flat;
+            classBtnPersonalizado2.ForeColor = Color.White;
+            classBtnPersonalizado2.IconAlignment = ContentAlignment.MiddleLeft;
+            classBtnPersonalizado2.IconChar = FontAwesome.Sharp.IconChar.None;
+            classBtnPersonalizado2.IconColor = Color.White;
+            classBtnPersonalizado2.IconPadding = 5;
+            classBtnPersonalizado2.IconSize = 24;
+            classBtnPersonalizado2.Location = new Point(40, 128);
+            classBtnPersonalizado2.Name = "classBtnPersonalizado2";
+            classBtnPersonalizado2.Size = new Size(150, 40);
+            classBtnPersonalizado2.TabIndex = 13;
+            classBtnPersonalizado2.Text = "Limpiar";
+            classBtnPersonalizado2.TextColor = Color.White;
+            classBtnPersonalizado2.UseVisualStyleBackColor = false;
+            // 
+            // classBtnPersonalizado1
+            // 
+            classBtnPersonalizado1.BackColor = Color.MediumSlateBlue;
+            classBtnPersonalizado1.BackgroundColor = Color.MediumSlateBlue;
+            classBtnPersonalizado1.BorderColor = Color.PaleVioletRed;
+            classBtnPersonalizado1.BorderRadius = 40;
+            classBtnPersonalizado1.BorderSize = 0;
+            classBtnPersonalizado1.FlatAppearance.BorderSize = 0;
+            classBtnPersonalizado1.FlatStyle = FlatStyle.Flat;
+            classBtnPersonalizado1.ForeColor = Color.White;
+            classBtnPersonalizado1.IconAlignment = ContentAlignment.MiddleLeft;
+            classBtnPersonalizado1.IconChar = FontAwesome.Sharp.IconChar.None;
+            classBtnPersonalizado1.IconColor = Color.White;
+            classBtnPersonalizado1.IconPadding = 5;
+            classBtnPersonalizado1.IconSize = 24;
+            classBtnPersonalizado1.Location = new Point(40, 82);
+            classBtnPersonalizado1.Name = "classBtnPersonalizado1";
+            classBtnPersonalizado1.Size = new Size(150, 40);
+            classBtnPersonalizado1.TabIndex = 12;
+            classBtnPersonalizado1.Text = "Agregar al carrito";
+            classBtnPersonalizado1.TextColor = Color.White;
+            classBtnPersonalizado1.UseVisualStyleBackColor = false;
+            classBtnPersonalizado1.Click += classBtnPersonalizado1_Click;
+            // 
+            // classBtnPersonalizado3
+            // 
+            classBtnPersonalizado3.BackColor = Color.MediumSlateBlue;
+            classBtnPersonalizado3.BackgroundColor = Color.MediumSlateBlue;
+            classBtnPersonalizado3.BorderColor = Color.PaleVioletRed;
+            classBtnPersonalizado3.BorderRadius = 40;
+            classBtnPersonalizado3.BorderSize = 0;
+            classBtnPersonalizado3.FlatAppearance.BorderSize = 0;
+            classBtnPersonalizado3.FlatStyle = FlatStyle.Flat;
+            classBtnPersonalizado3.ForeColor = Color.White;
+            classBtnPersonalizado3.IconAlignment = ContentAlignment.MiddleLeft;
+            classBtnPersonalizado3.IconChar = FontAwesome.Sharp.IconChar.None;
+            classBtnPersonalizado3.IconColor = Color.White;
+            classBtnPersonalizado3.IconPadding = 5;
+            classBtnPersonalizado3.IconSize = 24;
+            classBtnPersonalizado3.Location = new Point(26, 31);
+            classBtnPersonalizado3.Name = "classBtnPersonalizado3";
+            classBtnPersonalizado3.Size = new Size(165, 65);
+            classBtnPersonalizado3.TabIndex = 14;
+            classBtnPersonalizado3.Text = "Facturar";
+            classBtnPersonalizado3.TextColor = Color.White;
+            classBtnPersonalizado3.UseVisualStyleBackColor = false;
+            classBtnPersonalizado3.Click += classBtnPersonalizado3_Click;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(comboBox3);
+            groupBox3.Controls.Add(label12);
+            groupBox3.Controls.Add(classBtnPersonalizado3);
+            groupBox3.Location = new Point(686, 375);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(217, 179);
+            groupBox3.TabIndex = 15;
+            groupBox3.TabStop = false;
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(26, 138);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(146, 23);
+            comboBox3.TabIndex = 17;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(6, 110);
+            label12.Name = "label12";
+            label12.Size = new Size(49, 15);
+            label12.TabIndex = 14;
+            label12.Text = "Metodo";
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(670, 60);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(151, 23);
+            comboBox2.TabIndex = 16;
+            // 
+            // comboBox4
+            // 
+            comboBox4.FormattingEnabled = true;
+            comboBox4.Location = new Point(168, 58);
+            comboBox4.Name = "comboBox4";
+            comboBox4.Size = new Size(223, 23);
+            comboBox4.TabIndex = 18;
             // 
             // FormVentas
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(942, 581);
-            Controls.Add(label1);
+            Controls.Add(comboBox4);
+            Controls.Add(comboBox2);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(dataGridView1);
             Name = "FormVentas";
             Text = "FormVentas";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private Label label1;
+        private DataGridView dataGridView1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private controlesPersonalizados.ClassBtnPersonalizado classBtnPersonalizado2;
+        private controlesPersonalizados.ClassBtnPersonalizado classBtnPersonalizado1;
+        private controlesPersonalizados.ClassBtnPersonalizado classBtnPersonalizado3;
+        private ComboBox comboBox1;
+        private GroupBox groupBox3;
+        private Label label12;
+        private ComboBox comboBox2;
+        private controlesPersonalizados.Texboxs texboxs4;
+        private controlesPersonalizados.Texboxs texboxs3;
+        private controlesPersonalizados.Texboxs texboxs5;
+        private ComboBox comboBox3;
+        private ComboBox comboBox4;
     }
 }
